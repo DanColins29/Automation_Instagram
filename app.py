cef8a72cc3c1af2d317aa2c2498c27249d0a2bd2
Danoninho29 <danielcolins29@gmail.com>
2021-05-26 11:05:42 -0300

    version 0.3

--- app.py
+++ app.py
@@ -8,13 +8,13 @@ import time
 def add_post():
     bot = Client()
 
-    bot.login(username="apenas_conta", password="Colinsd29@")
-    album = ["img/note1.jpeg", "img/note.jpeg"]
-    cap = "Teste \n#teste #conta"
+    bot.login(username="bfalbertomersmann", password="*****")
+    album = ["img/liberte.jpeg", "img/auto.jpeg"]
+    cap = "Indicações de leitura do maranhense Padre João Mohana disponíveis para empréstimo.\n\nLiberte seu filho da insegurança e Autoanálise para o êxito profissional\n\nAgora me conta aí, qual desses vocês já conheciam ou que vai para sua lista de futura leitura?\n\n#leitura #bibliotecaiesma #iesma #padrejoaomohana #joaomohana #teologia"
 
     bot.album_upload(album, caption = cap)
 
-schedule.every(1).minutes.do(add_post)
+schedule.every().wednesday.at("10:09").do(add_post)
 
 count = 0
 
