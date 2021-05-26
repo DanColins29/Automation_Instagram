312237a62959afb09f783396b69f9d1abe2e72cd
Danoninho29 <danielcolins29@gmail.com>
2021-05-26 08:10:47 -0300

    version 0.2

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Automation_Instagram
+
+This App was created to automate posts of the Instagram Social Media.
+
+It's my first App about Automation.
+
+HOW TO USE:
+
+1. In the fields login and password, insert the authentication of your Instagram Account;
+2. Put the images that you will post into the fold /img;
+3. In the variable that contains the reference of your images, insert the strings who indicate your path photos;
--- app.py
+++ app.py
@@ -8,14 +8,20 @@ import time
 def add_post():
     bot = Client()
 
-    bot.login(username="apenas_conta", password="****")
+    bot.login(username="apenas_conta", password="Colinsd29@")
     album = ["img/note1.jpeg", "img/note.jpeg"]
     cap = "Teste \n#teste #conta"
 
     bot.album_upload(album, caption = cap)
 
-schedule.every(5).minute.do(add_post)
+schedule.every(1).minutes.do(add_post)
+
+count = 0
 
 while 1:
     schedule.run_pending()
     time.sleep(1)
+    
+    count+=1
+    
+    print("waiting ", count, " seconds")
