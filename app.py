946dd160d4c6db4e634759e7992b334782942944
Danoninho29 <danielcolins29@gmail.com>
2021-05-26 11:50:31 -0300

    version 0.3.1

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 # imports
+from enum import Flag
 from instagrapi import Client
 
 import schedule
@@ -8,17 +9,23 @@ import time
 def add_post():
     bot = Client()
 
-    bot.login(username="bfalbertomersmann", password="*****")
+    bot.login(username = user, password = pwd)
     album = ["img/liberte.jpeg", "img/auto.jpeg"]
     cap = "Indicações de leitura do maranhense Padre João Mohana disponíveis para empréstimo.\n\nLiberte seu filho da insegurança e Autoanálise para o êxito profissional\n\nAgora me conta aí, qual desses vocês já conheciam ou que vai para sua lista de futura leitura?\n\n#leitura #bibliotecaiesma #iesma #padrejoaomohana #joaomohana #teologia"
 
     bot.album_upload(album, caption = cap)
 
-schedule.every().wednesday.at("10:09").do(add_post)
-
+# Variables
 count = 0
+flag = False
+user = "apenas_conta"
+pwd = "Colinsd29@"
 
-while 1:
+# Call Function By Schedule
+schedule.every().wednesday.at("11:33").do(add_post)
+
+# Loop
+while (flag == False):
     schedule.run_pending()
     time.sleep(1)
     
